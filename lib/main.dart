e2d8da79b33cbf431e20884ab47242ded257fcef
Thomas B <16827920+buehren@users.noreply.github.com>
2021-03-08 16:55:15 +0000

    Add WebSocketChannel

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,36 @@
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:args/args.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    const String _webSocketUrl = String.fromEnvironment('WS_URL', defaultValue: 'ws://echo.websocket.org/');
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Websocket Client',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Websocket Client', webSocketUrl: _webSocketUrl),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
+  final String webSocketUrl;
+
+  MyHomePage({Key? key, required this.title, required this.webSocketUrl})
+      : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -47,67 +38,99 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  StreamController<String> _streamController = StreamController<String>();
+  WebSocketChannel? _channel;
+  String _lastReceivedMessage = "";
+  bool _hasStartedConnect = false;
+  bool _isSocketOpen = false;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _isSocketOpen = false;
+    _connect();
+  }
+
+  void _sendMessage() {
+    _counter++;
+    print("Sending message #$_counter to WebSocket sink");
+    _channel?.sink.add("Message #$_counter from Flutter to WebSocket");
+  }
+
+  _wserror(err) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isSocketOpen = false;
     });
+
+    print(new DateTime.now().toString() + " Connection error: $err");
+    await _connect();
+  }
+
+  _wsdone() async {
+    setState(() {
+      _isSocketOpen = false;
+    });
+
+    print(new DateTime.now().toString() + " Connection done");
+    await _connect();
+  }
+
+  _connect() async {
+    if (_hasStartedConnect) {
+      // reconnect delay
+      await Future.delayed(Duration(seconds: 4));
+    }
+
+    setState(() {
+      print(new DateTime.now().toString() +
+          " Starting connection attempt to " +
+          widget.webSocketUrl +
+          " ...");
+
+      _channel = WebSocketChannel.connect(Uri.parse(widget.webSocketUrl));
+
+      _hasStartedConnect = true;
+    });
+
+    _channel?.stream.listen(
+        (data) {
+            print("Received data: $data");
+            setState(() {
+              _isSocketOpen = true;
+              _lastReceivedMessage = data;
+            });
+            _streamController.add(data);
+        },
+        onDone: _wsdone,
+        onError: _wserror,
+        cancelOnError: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Last received message from ${widget.webSocketUrl}:',
             ),
             Text(
-              '$_counter',
+              '$_lastReceivedMessage',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: Icon(Icons.send),
+      ),
     );
   }
 }
